72e4b855154d5a48822da1e6e3bad15b37540e89
Aymeric Issika <134638831+issikaaymeric@users.noreply.github.com>
2026-03-29 13:22:06 +0200

    Update Column.jsx

--- frontend/src/components/Column.jsx
+++ frontend/src/components/Column.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Droppable } from '@hello-pangea/dnd';
 import styled from 'styled-components';
 import Task from './Task.jsx';
